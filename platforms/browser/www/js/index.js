2818507aac9e281ca7b953eeabe55f2c55d27daf
Sharvan <itbhu.sk@gmail.com>
2018-05-17 09:29:14 +0530

    device testingwwer

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -56,6 +56,7 @@ var app = {
         push.on('registration', function(data) {
             console.log('registration event: ' + data.registrationId);
             alert(data.registrationId);
+            localStorage.clear();
             var oldRegId = localStorage.getItem('registrationId');
             if (oldRegId !== data.registrationId) {
                 // Save new registration ID
